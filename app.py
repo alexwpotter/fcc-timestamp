b14cccc49d916b9d3c1bc65c371b37338746c9c1
apotter <alexwpotter@gmail.com>
2016-10-05 03:03:07 -0400

    fixed url stuff

--- app.py
+++ app.py
@@ -3,20 +3,28 @@ import time,re,urllib
 app = Flask(__name__)
 chop_time = lambda x: int(str(x)[:-3])
 @app.route('/<ts>')
-def index(ts):
+def index(ts=None):
 	ts = str(urllib.unquote(ts))
-	if ts:
+	if ts != None:
 		if re.match(r'[A-z].+',ts) != None:
 			natural = ts
 			unix = chop_time(time.mktime(time.strptime(ts,'%B %d, %Y')))
 		else:
 			unix = ts
 			natural = time.strftime('%B %d, %Y',time.localtime(float(ts)))
-		resp = make_response(str({'unix': unix,'natural': natural}),200)
-		resp.headers['Content-Type'] = 'application/json'
-		return resp
 	else:
-		return 'null'
+		unix = 'null'
+		natural = 'null'
+
+	resp = make_response(str({'unix': unix,'natural': natural}),200)
+	resp.headers['Content-Type'] = 'application/json'
+	return resp
+
+@app.route('/')
+def ndx():
+	resp = make_response(str({'unix': 'null','natural': 'null'}),200)
+	resp.headers['Content-Type'] = 'application/json'
+	return resp
 
 if __name__ == "__main__":
-	app.run("0.0.0.0")
+	app.run(host="0.0.0.0",port=80)
